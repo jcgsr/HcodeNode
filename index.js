5aa0d9747c35f6df120ae8a5dbed0e8f14849900
jcgsr <cabralguerra@gmail.com>
2020-05-12 18:03:37 -0300

    other fix

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const consign = require('consign');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 
-
 let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
